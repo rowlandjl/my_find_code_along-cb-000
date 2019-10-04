fe11a04412723591d715b337fd28a3cccb8b1aa5
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-04 15:45:29 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,9 +3,9 @@ require 'pry'
 def my_find(collection)
   i = 0
   while i < collection.length
-    if yield(collection[i])
+    return collection[i] if yield(collection[i])
       return collection[i]
-    end 
+    end
     i += 1
   end
 end
